3ef5914635f87243eccbd9f78caf708345b55f53
ANANYA RAO <79102084+Ananyarao610@users.noreply.github.com>
2023-09-04 10:32:24 +0530

    Update login2.js

--- login2.js
+++ login2.js
@@ -1,21 +1,34 @@
-function validate()
-{
-    var username=document.getElementById("username").value;
-    var password=document.getElementById("password").value;
-    if((username==="ananya"&&password==="ananya")||(username==="vinay"&&password==="vinay")||(username==="qwe"&&password==="qwe"))
-    {
-        alert("login successful");
-        return false;
-    }
-    if(username!=="ananya"|| username!=="vinay"||username!=="qwe")
-    {
-        alert("username incorrect");
-document.write('username incorrect');
-    }
-    if(password!=="ananya"|| password!=="vinay"||password!=="qwe")
-    {
-        alert("password incorrect");
-        document.write('password incorrect');
-    }
-    
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Create a data object to send to the server
+    var data = {
+        username: username,
+        password: password
+    };
+
+    // Make an AJAX request to the server for validation
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/validate_user.php", true); // Replace with your server-side script URL
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+
+                if (response.success) {
+                    alert("Login successful");
+                    // Redirect to a dashboard or another page
+                } else {
+                    alert("Username or password incorrect");
+                }
+            } else {
+                alert("Server error. Please try again later.");
+            }
+        }
+    };
+
+    xhr.send(JSON.stringify(data));
 }
